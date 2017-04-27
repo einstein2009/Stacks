2be6a04159391bc9e7c3ad5711098ea302a5988c
Nick Ackors <einstein_n_2009@yahoo.com>
2017-04-26 20:50:32 -0500

    further improvements to Calculator.

--- Week 4 Written Assignment/Week 4 Written Assignment.cpp
+++ Week 4 Written Assignment/Week 4 Written Assignment.cpp
@@ -13,8 +13,8 @@ using namespace std;
 void Calculator(string input);
 void Execute(Stack<int> &i, Stack<char> &c);
 int Precedence(char ch);
-bool IsNumeric(string input);
-bool IsOperator(string input);
+//bool IsNumeric(string input);
+//bool IsOperator(string input);
 
 void Calculator(string input) {
 	Stack<int>* valStack = new Stack<int>();
@@ -25,18 +25,26 @@ void Calculator(string input) {
 
 		switch (input[i]) {
 			
-		case IsNumeric(input):
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9': valStack->push(input[i]);
+			break;
 
-			valStack->push(input[i]);
-				break;
-
-		case input[i] == "(":
-
+		case '(':
 			opStack->push(input[i]);
 				break;
 
-		case IsOperator(input):
-
+		case '+':
+		case '-':
+		case '*':
+		case '/':
 			if (opStack->isEmpty())
 				opStack->push(input[i]);
 			else if (Precedence(input[i]) > Precedence(opStack->peek()))
@@ -48,7 +56,7 @@ void Calculator(string input) {
 			}
 				break;
 
-		case input[i] == ')':
+		case ')':
 
 			while (opStack->peek() == '(')
 				Execute();
@@ -57,7 +65,7 @@ void Calculator(string input) {
 		}
 	}
 	while (!opStack->isEmpty()) {
-		Execute(valStack, opStack);
+		Execute();
 		result = valStack->peek();
 	}
 };
@@ -120,7 +128,7 @@ int Precedence(char ch) {
 
 	return;
 }
-
+/*
 bool IsOperator(string input)
 {
 	bool verify = true;
@@ -149,6 +157,7 @@ bool IsNumeric(string input)
 	}
 	return verify;
 }
+*/
 
 int main()
 {
